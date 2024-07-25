ddc080cc7e13f58b3e4e7f54c9e2fdc3e690e660
Evgeniy <evgnec@gmail.com>
2024-07-25 23:08:54 +0300

    upd

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,11 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
 const myInput = document.getElementById('datetime-picker');
-// myInput.value=Date.now();
+myInput.value=Date.now();
 const btnStart = document.querySelector('button[data-start]');
+let isActive = false;
 
 flatpickr("#datetime-picker",{
     enableTime: true,
@@ -19,7 +21,9 @@ flatpickr("#datetime-picker",{
 btnStart.addEventListener('click', startTimer);
 
 function startTimer() {
- console.log("myInput.value", myInput.value)
+    if (isActive) {return;}
+    console.log("myInput.value", myInput.value)
+    isActive = true;
 }
 
 
