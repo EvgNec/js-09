2fa01a3b7503c7c74f62e13a65c94f418ceb56f6
Evgeniy <evgnec@gmail.com>
2024-07-25 14:53:49 +0300

    upd

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,25 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+
+const myInput = document.getElementById('datetime-picker');
+// myInput.value=Date.now();
+const btnStart = document.querySelector('button[data-start]');
+
+flatpickr("#datetime-picker",{
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    minDate: "today",
+        "disable": [
+        function(date) {
+            // return true to disable
+            return (date.getDay() === 0 || date.getDay() === 6);
+        }],
+});
+
+btnStart.addEventListener('click', startTimer);
+
+function startTimer() {
+ console.log("myInput.value", myInput.value)
+}
+
+
