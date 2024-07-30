d2959a9ab1a99bdf552e031d01400d9b08687ed5
Evgeniy <evgnec@gmail.com>
2024-07-30 23:22:03 +0300

    upd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,12 +7,22 @@
 //   }
 // }
 
-const fetchPockemon = id => {
-fetch(`https://pokeapi.co./api/v2/pokemon/${id}`)
-  .then(r => r.json())
-  .then(res => console.log(res))
-.catch(err => console.log(err));
+// const fetchPockemon = (id, onSuccess, onErrror) => {
+// fetch(`https://pokeapi.co./api/v2/pokemon/${id}`)
+//   .then(r => r.json())
+//   .then(pokemon => { onFetchSuccess(pokemon) })
+//   .catch(error => { onFetchError(err) });
+// }
+
+const fetchPockemon = (id) => {
+return fetch(`https://pokeapi.co./api/v2/pokemon/${id}`).then(r => r.json())
 }
 
+// fetchPockemon(2, onFetchSuccess, onFetchError);
+fetchPockemon(2).then(onFetchSuccess).catch(onFetchError);
 
-fetchPockemon(2);
+function onFetchSuccess(pokemon) {
+  console.log("pokemon", pokemon);  
+};
+
+function onFetchError(err) { console.log(err); };
