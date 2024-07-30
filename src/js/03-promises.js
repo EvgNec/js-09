778331c25b7984a23e2042d87a0c27ecd2b0a72c
Evgeniy <evgnec@gmail.com>
2024-07-30 23:11:49 +0300

    upd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,18 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+const fetchPockemon = id => {
+fetch(`https://pokeapi.co./api/v2/pokemon/${id}`)
+  .then(r => r.json())
+  .then(res => console.log(res))
+.catch(err => console.log(err));
 }
+
+
+fetchPockemon(2);
