586b30f1dd68228e0fb214d06c47147d4b3e86d1
EvgNec <evgnec@gmail.com>
2024-07-30 20:12:12 +0000

    Deploying to gh-pages from @ EvgNec/js-09@778331c25b7984a23e2042d87a0c27ecd2b0a72c 🚀

---
+++ 03-promises.dffd69b0.js
@@ -0,0 +1,2 @@
+fetch("https://pokeapi.co./api/v2/pokemon/".concat(2)).then((function(n){return n.json()})).then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}));
+//# sourceMappingURL=03-promises.dffd69b0.js.map
