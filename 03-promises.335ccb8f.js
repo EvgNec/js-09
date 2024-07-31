11260eb4952a380f2011c34c131e098119e4f0dc
EvgNec <evgnec@gmail.com>
2024-07-31 12:55:13 +0000

    Deploying to gh-pages from @ EvgNec/js-09@edd66583ab7de828882f3bb7a163741bb83535bf 🚀

--- 03-promises.335ccb8f.js
+++
@@ -1,2 +0,0 @@
-var o;(o=2,fetch(`https://pokeapi.co./api/v2/pokemon/${o}`).then((o=>o.json()))).then((function(o){console.log("pokemon",o)})).catch((function(o){console.log(o)}));
-//# sourceMappingURL=03-promises.335ccb8f.js.map
--- 03-promises.9c0da996.js
+++
@@ -1,2 +0,0 @@
-!function(){var o;(o=2,fetch("https://pokeapi.co./api/v2/pokemon/".concat(o)).then((function(o){return o.json()}))).then((function(o){console.log("pokemon",o)})).catch((function(o){console.log(o)}))}();
-//# sourceMappingURL=03-promises.9c0da996.js.map
