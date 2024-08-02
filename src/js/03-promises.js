7aed368449c33e8d8c66d47abb91fcc3eacf605c
Evgeniy <evgnec@gmail.com>
2024-08-02 23:37:00 +0300

    upd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,18 @@
+import Notiflix from 'notiflix';
 
 const refs = {
+stBtn: document.querySelector('[data-dsdd]'),
 
-  
 
 }
 
+Notiflix.Notify.success('Sol lucet omnibus');
+
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+Notiflix.Notify.warning('Memento te hominem esse');
+
+Notiflix.Notify.info('Cogito ergo sum');
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
