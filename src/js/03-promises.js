bf827c6ec501597c85ef699bc3b262a6492823f0
Evgeniy <evgnec@gmail.com>
2024-08-03 00:33:12 +0300

    upd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,19 +1,35 @@
 import Notiflix from 'notiflix';
 
-const refs = {
-  delayInp: document.querySelector("input[name=delay]"),
-  stepInp: document.querySelector("input[name=step]"),
-  amountInp: document.querySelector("input[name=amount]"),
-  formSubmit: document.querySelector(".form"),
-}
+// const refs = {
+//   delayInp: document.querySelector("input[name=delay]"),
+//   stepInp: document.querySelector("input[name=step]"),
+//   amountInp: document.querySelector("input[name=amount]"),
+//   // formSubmit: document.querySelector(".form"),
+// }
+const form = document.querySelector('.form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  formData.forEach((value, name) => {
+    console.log("name", name);
+    console.log("value", value);
+  })
+})
 
-Notiflix.Notify.success('Sol lucet omnibus');
+// function test() {
 
-Notiflix.Notify.failure('Qui timide rogat docet negare');
+//  }
 
-Notiflix.Notify.warning('Memento te hominem esse');
 
-Notiflix.Notify.info('Cogito ergo sum');
+
+// Notiflix.Notify.success('Sol lucet omnibus');
+
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+// Notiflix.Notify.warning('Memento te hominem esse');
+
+// Notiflix.Notify.info('Cogito ergo sum');
+
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
