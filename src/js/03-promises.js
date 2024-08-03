566e69a347932092418166a2a6a048b9df2c39af
Evgeniy <evgnec@gmail.com>
2024-08-03 16:09:35 +0300

    upd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,41 +1,21 @@
 import Notiflix from 'notiflix';
 
-// const refs = {
-//   delayInp: document.querySelector("input[name=delay]"),
-//   stepInp: document.querySelector("input[name=step]"),
-//   amountInp: document.querySelector("input[name=amount]"),
-//   // formSubmit: document.querySelector(".form"),
-// }
-const form = document.querySelector('.form')
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-  const formData = new FormData(e.currentTarget);
-  formData.forEach((value, name) => {
-    console.log("name", name);
-    console.log("value", value);
+const refs = {
+  delayInp: document.querySelector("input[name=delay]"),
+  stepInp: document.querySelector("input[name=step]"),
+  amountInp: document.querySelector("input[name=amount]"),
+form: document.querySelector('.form'),
+}
+refs.form.addEventListener('submit', run);
+
+
+function createPromise(position, newDelay) {
+  const shouldResolve = Math.random() > 0.5;
+    return new Promise((resolve, reject) => {
+        if (shouldResolve) {
+         return resolve({position, newDelay})
+        } else {
+         return reject({position, newDelay})
+        }
   })
-})
-
-// function test() {
-
-//  }
-
-
-
-// Notiflix.Notify.success('Sol lucet omnibus');
-
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
-
-// Notiflix.Notify.warning('Memento te hominem esse');
-
-// Notiflix.Notify.info('Cogito ergo sum');
-
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-    }
+}
