b7339bc8a674e9bb04b8077f8f0c6976d01d079d
EvgNec <evgnec@gmail.com>
2024-08-07 16:06:38 +0000

    Deploying to gh-pages from @ EvgNec/js-09@75a3c1b9a864f27212462d0744eb0c82eda5903f 🚀

--- 03-promises.cc4ed70c.js
+++
@@ -1,2 +0,0 @@
-console.log("Starting server"),new Promise((function(e,n){setTimeout((function(){console.log("Preparing server..."),e({server:"aws",port:1234,status:"running"})}),2e3)})).then((function(e){new Promise((function(n,o){setTimeout((function(){e.modified=!0,n(e)}),2e3)})).then((function(e){console.log("Data received",e)}))}));
-//# sourceMappingURL=03-promises.cc4ed70c.js.map
